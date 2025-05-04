6492bc5887536d48b2f968c592b745865877b55e
MistaCareca <brennno0cartaxo@gmail.com>
2025-05-04 17:06:11 -0300

    Feat: valores do user, lista de moedas
    task 8, 9 e 10

--- src/main/com/conversordemoeda/Cliente_api.java
+++ src/main/com/conversordemoeda/Cliente_api.java
@@ -7,24 +7,44 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Cliente_api {
+        private static void coverterValor(ConversaoResponse conversao, double valor, Scanner sc){
+            if(valor < 0){
+                System.out.println("Erro: O valor a converter dever ser positivo.");
+                return;
+            }
+            double resultado = valor * conversao.getConversionRate();
+            System.out.printf("Resultado de conversao: %.2f %s = %.2f %s%n",
+                    valor, conversao.getBaseCode(), resultado, conversao.getTargetCode());
+        }
+
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient cliente = HttpClient.newHttpClient();
         Scanner sc = new Scanner(System.in);
         Gson gson = new Gson();
 
-        System.out.println("Informe a sigla da moeda de origem (ex: USD): ");
-        String moedaOrigem = sc.nextLine().toUpperCase();
-        System.out.println("Informe a sigla da moeda de destino (ex: BRL): ");
-        String moedaDestino = sc.nextLine().toUpperCase();
+        List<String> moedasPermitidas = Arrays.asList("ARS", "BOB", "BRL", "CLP", "USD", "COP");
 
-        String apiKey = "";
-        String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s",
-                apiKey, moedaOrigem, moedaDestino);
+        try{
+            System.out.println("Moedas disponiveis: ARS, BOB, BRL, CLP, USD, COP");
+            System.out.println("Informe a sigla da moeda de origem (ex: USD): ");
+            String moedaOrigem = sc.nextLine().toUpperCase();
+            System.out.println("Informe a sigla da moeda de destino (ex: BRL): ");
+            String moedaDestino = sc.nextLine().toUpperCase();
 
-        HttpRequest request = HttpRequest.newBuilder()
+            if(!moedasPermitidas.contains(moedaOrigem) || !moedasPermitidas.contains(moedaDestino)){
+                System.out.println("Erro: Moeda inválida. Escolha entre: " + moedasPermitidas);
+                return;
+            }
+            String apiKey = "";
+            String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s",
+                    apiKey, moedaOrigem, moedaDestino);
+
+            HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
                 .header("Accept", "application/json")
@@ -32,14 +52,40 @@ public class Cliente_api {
                 .timeout(Duration.ofSeconds(10))
                 .build();
 
-        HttpResponse<String> response = cliente.send(request,
+            HttpResponse<String> response = cliente.send(request,
                 HttpResponse.BodyHandlers.ofString());
 
-        ConversaoResponse conversao = gson.fromJson(response.body(), ConversaoResponse.class);
-        System.out.println("\n=== Resultados (Mapeamento com Gson) ===");
-        System.out.printf("Taxa de câmbio: 1 %s = %.4f %s%n",
-                conversao.getBaseCode(), conversao.getConversionRate(), conversao.getTargetCode());
+            if(response.statusCode() != 200){
+                System.out.println("Erro na requisiçao: codigo: " + response.statusCode());
+                return;
+            }
+            ConversaoResponse conversao = gson.fromJson(response.body(), ConversaoResponse.class);
+            System.out.println("\n=== Resultados (Mapeamento com Gson) ===");
+            if (conversao.getErrorType() != null){
+                System.out.println("Erro da API: " + conversao.getErrorType());
+                return;
+            }
+            System.out.printf("Taxa de câmbio: 1 %s = %.4f %s%n",
+                    conversao.getBaseCode(), conversao.getConversionRate(), conversao.getTargetCode());
 
+            System.out.println("\nInforme o valor a covnerter (ex:5.50): ");
+            double valor;
+            try{
+                valor = sc.nextDouble();
+            }  catch (Exception e){
+                System.out.println("Erro: valor invalido. use numeros (ex: 5.50)");
+                return;
+            }
+
+            coverterValor(conversao, valor, sc);
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Erro ao fazer a requisiçao: " + e.getMessage());
+        } catch (com.google.gson.JsonSyntaxException e) {
+            System.out.println("Erro: Formato JSON invalido na resposta");
+        } finally {
         sc.close();
+        }
     }
+
 }
--- src/main/com/conversordemoeda/ConversaoResponse.java
+++ src/main/com/conversordemoeda/ConversaoResponse.java
@@ -4,6 +4,7 @@ public class ConversaoResponse {
     private String base_code;
     private String target_code;
     private double conversion_rate;
+    private String error_type;
 
     public String getBaseCode() {
         return base_code;
@@ -15,5 +16,9 @@ public class ConversaoResponse {
 
     public String getTargetCode() {
         return target_code;
+    }
+
+    public String getErrorType() {
+        return error_type;
     }
 }
